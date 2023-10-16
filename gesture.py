eed0fac88131b48c61ba2bac23eb5422cbd1203e
sahiba.awan <sahibaawan123@gmail.com>
2023-10-16 09:36:45 -0700

    first commit

---
+++ gesture.py
@@ -0,0 +1,7 @@
+import numpy as np 
+from tensorflow.keras.models import sqeuential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
